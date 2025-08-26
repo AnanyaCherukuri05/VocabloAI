6a1fabf9410def86c61edfb19410f6ee46817f67
AnanyaCherukuri05 <ananyachrukuri5@gmail.com>
2025-08-26 17:15:11 +0530

    sysUserPrompt

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import zeroShotRouter from "./routes/zeroShot.js";
+import systemUserPrompt from "./routes/systemUserPrompt.js";
+
 
 dotenv.config();
 
@@ -11,6 +13,7 @@ app.use(express.json());
 
 // Routes
 app.use("/api/zero-shot", zeroShotRouter);
+app.use("/api/systemuser", systemUserPrompt);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
